a71da36fbcf586274914b57050609bcc54b61aa2
James Fox-Collis <jlfoxcollis@gmail.com>
2020-10-17 12:06:17 -0700

    Player created and tested for name and position

---
+++ lib/player.rb
@@ -0,0 +1,12 @@
+class Player
+
+  attr_reader :name, :position
+
+  def initialize(name:, position:)
+    @name = name
+    @position = position
+  end
+
+
+
+end
---
+++ test/player_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/player'
+
+class PlayerTest < MiniTest::Test
+
+  def test_it_exists
+    player = Player.new({name: "Luka Modric", position: "midfielder"})
+
+    assert_equal "Luka Modric", player.name
+    assert_equal "midfielder", player.position
+  end
+
+end
